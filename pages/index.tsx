dc1bf03a41e9333d306e5dad178a4b46a8496cec
lylaw27 <lylaw27@gmail.com>
2022-12-14 08:07:35 +0800

    first

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 // components/ProductCollection.tsx
 import React from "react";
+import Image from "next/image";
 import {Product, useProductGetListQuery } from "@/saleor/api";
 import Navbar from "@/components/navbar";
 import ProductList from "@/components/productList";
@@ -13,6 +14,15 @@ export default function Home() {
     return (
       <>
       <Navbar/>
+          <div className="carousel w-full h-screen">
+      <div id="slide1" className="carousel-item relative w-full">
+        <Image fill alt="" src="/images/carousel.jpg" className="w-full"/>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">❮</a>
+          <a href="#slide2" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+      </div>
       <div className="flex">
           {productList?.length > 0 &&
           productList.map(({ node }) => (
