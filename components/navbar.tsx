cf78db12c1cf3f56be0c7e2cb0f99c906d060366
lylaw27 <lylaw27@gmail.com>
2022-12-28 07:32:43 +0800

    first

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,24 +1,19 @@
 // components/ProductCollection.tsx
-import { useProductUpdateVariantInCartMutation, CheckoutLine } from "@/saleor/api";
+import { useProductUpdateVariantInCartMutation, useCheckoutFetchByTokenQuery, CheckoutLine } from "@/saleor/api";
 import Image from "next/image";
 import {useLocalStorage} from "react-use";
-import { useContext } from "react";
-import { CartContext } from "pages/_app";
+import {useState} from 'react';
 
 export default function Navbar() {
-  const {cartContent,setCartContent} = useContext(CartContext);
   const [token] = useLocalStorage('token');
+  const { data, loading, error } = useCheckoutFetchByTokenQuery({variables: { checkoutToken: token },skip: !token});
+  const cartList = data?.checkout?.lines || [];
   const [updateProductInCart] = useProductUpdateVariantInCartMutation();
-  const updateCart = async(item: CheckoutLine) =>{
+  const updateCart = async(id: string,quantity: number) =>{
     await updateProductInCart({
-      variables:{ checkoutToken: token, lineId: item.id, quantity: item.quantity}
+      variables:{ checkoutToken: token, lineId: id, quantity: quantity}
     });
-    let cartIndex = cartContent.findIndex((obj => obj.id === item.id));
-    let cartNewList = cartContent
-    cartNewList[cartIndex] = item;
-    setCartContent(cartNewList);
   }
-  
  return(
   <div className="navbar bg-white p-0">
   <div className="flex-1 px-10">
@@ -45,13 +40,13 @@ export default function Navbar() {
       <label tabIndex={0} className="btn btn-ghost btn-circle">
         <div className="indicator">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-          <span className="badge badge-sm indicator-item">{cartContent.length}</span>
+          <span className="badge badge-sm indicator-item">8</span>
         </div>
       </label>
       <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-80 bg-base-100 shadow">
         <div className="card-body">
-          <span className="font-bold text-lg">{cartContent.length} Items</span>
-          {cartContent.map((items) =>
+          <span className="font-bold text-lg">{cartList.length} Items</span>
+          {cartList.map((items)=>
             <div key={items?.variant?.product?.id} className="flex items-center h-16">
               <div className="relative w-1/4 h-16">
                 <Image src={items?.variant?.product?.thumbnail!.url} fill alt=""/>
@@ -59,13 +54,13 @@ export default function Navbar() {
               <div className="p-7 w-2/4">
                 {items?.variant?.product?.name}
               </div>
-              <button onClick={() => updateCart(items as CheckoutLine)} className="btn btn-circle btn-outline border-box w-8 h-8 min-h-0">
+              <button onClick={() => updateCart(items.id,items.quantity-1)} className="btn btn-circle btn-outline border-box w-8 h-8 min-h-0">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M0 12L24 12" /></svg>
               </button>
               <div className="w-10 text-center">
               {items?.quantity}
               </div>
-              <button onClick={() => updateCart(items as CheckoutLine)} className="btn btn-circle btn-outline border-box w-8 h-8 min-h-0">
+              <button onClick={() => updateCart(items.id,items.quantity+1)} className="btn btn-circle btn-outline border-box w-8 h-8 min-h-0">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M0 12L24 12M12 0L12 24" /></svg>
               </button>
             </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,15 @@
 import type { AppProps } from "next/app";
-import React, { useState, createContext } from 'react'
+import React from 'react'
 import { useLocalStorage, useEffectOnce } from "react-use";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Script from 'next/script';
 import "../styles/globals.css";
-import { useCheckoutCreateMutation, useCheckoutFetchByTokenQuery, CheckoutLine} from "@/saleor/api";
+import { useCheckoutCreateMutation, CheckoutLine} from "@/saleor/api";
 
 const client = new ApolloClient({
   uri: "https://natuprime.saleor.cloud/graphql/",
   cache: new InMemoryCache(),
 });
-
-type CheckoutLineType = {
-  cartContent: CheckoutLine[] ;
-  setCartContent: (value: CheckoutLine[]) => void;
-};
 
 const Root = ({ Component, pageProps}: AppProps)=>{
   const [token,setToken] = useLocalStorage("token");
@@ -22,40 +17,22 @@ const Root = ({ Component, pageProps}: AppProps)=>{
   async function doCheckout() {
     const {data} = await CheckoutCreate();
     const token = data?.checkoutCreate?.checkout?.token;
-    setToken(token)
+    setToken(token);
   }
   useEffectOnce(()=>{
     if(!token){
       doCheckout();
     }
   })
-
   return <Component {...pageProps} token={token}/>
 }
 
-const CartQuery = () =>{
-  const [token] = useLocalStorage('token');
-  const { data, loading, error } = useCheckoutFetchByTokenQuery({
-      variables: { checkoutToken: token },
-      skip: !token,
-    });
-    let cartList = data?.checkout?.lines as CheckoutLine[];
-    return cartList
-}
-
-export const CartContext = createContext<CheckoutLineType>({
-  cartContent: CartQuery(), setCartContent: () =>{}
-});
-
 export default function MyApp(props: AppProps) {
-  const [cartContent,setCartContent] = useState(CartQuery())
   return (
     <>
     <Script src="https://kit.fontawesome.com/dbb3bd5296.js" crossOrigin="anonymous"/>
     <ApolloProvider client={client}>
-    <CartContext.Provider value={{cartContent,setCartContent}}>
       <Root {...props} />
-    </CartContext.Provider>
     </ApolloProvider>
     </>
   );
